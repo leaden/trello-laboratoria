fdbf5a8a7e4f6b4ea52d3a589cf73746cd280eab
Maribel Diaz <maribel.diaz.j@gmail.com>
2017-05-18 07:23:01 -0300

    añadir lista ejercicio evaluado lccion 20

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+
+
+  function agregar() {
+    var x = document.createElement("TEXTAREA");
+    var t = document.createTextNode("Añadir tarea");
+    x.appendChild(t);
+    document.body.appendChild(x);
+}
+
